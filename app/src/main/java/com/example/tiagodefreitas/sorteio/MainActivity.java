a0637c237be911a4195ab3ae24b4c8cad4b7aba6
Tiago de Freitas <tiagoabc@gmail.com>
2023-02-06 21:07:39 -0300

    Adicionado o método    sortearNumeros

--- app/src/main/java/com/example/tiagodefreitas/sorteio/MainActivity.java
+++ app/src/main/java/com/example/tiagodefreitas/sorteio/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.tiagodefreitas.sorteio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumeros(View view){
+        TextView txt_Resultado = (TextView) findViewById(R.id.txt_Resultado);
+
+        //Sorteia um número alatório de 0 a 10
+        int n1 = new Random().nextInt(10);
+
+        txt_Resultado.setText("O número alatório é: " + n1);
     }
 }
